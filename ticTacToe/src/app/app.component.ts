3a5ea9770129ce6ee1752a85267f28ef2e517e4a
WINMAC12\2daw <gabrieldt007@hotmail.com>
2019-01-17 10:30:20 +0100

    Primera parte terminada

--- ticTacToe/src/app/app.component.ts
+++ ticTacToe/src/app/app.component.ts
@@ -7,15 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticTacToe';
-  turno : boolean = false;
+  casillas : number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  turno : boolean = false ;
+  clicados : number[] = [];
 
-  ponerMarca(id){
-    if (this.turno === false) {
-      document.getElementById('id').innerHTML= "<img src='../assets/FotoX.jpg'>";
-      this.turno = true;
-    } else {
-      document.getElementById('id').innerHTML= "<img src='../assets/FotoCirculo.jpg'>";
-      this.turno = false;
+
+  ponerMarca(num){
+    let result = this.clicados.some(pieza => pieza === num);
+
+    console.log(result);
+
+    if (!result) {
+      this.clicados.push(num);
+
+      if (this.turno === false) {
+        this.casillas[num] = 2;
+        this.turno = true;
+      } else {
+        this.casillas[num] = 1;
+        this.turno = false;
+      }
     }
+
   }
 }
