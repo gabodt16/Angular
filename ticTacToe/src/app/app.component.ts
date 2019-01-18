fc90ea78de2be07261df3757671dabfb08af8995
DESKTOP-7Q4F35D\gabri <vivalavidaloca@gmail.com>
2019-01-18 03:32:12 +0100

    funciona correctamente el sistema de victoria y los mensajes del ganador

--- ticTacToe/src/app/app.component.ts
+++ ticTacToe/src/app/app.component.ts
@@ -7,27 +7,90 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticTacToe';
-  casillas : number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  turno : boolean = false ;
+  urls : string[] = ["", "", "", "", "", "", "", "", ""];
+  viejas : string[] = ["../assets/vieja2.gif", "../assets/vieja1.jpg", "../assets/vieja3.gif", "../assets/vieja4.gif", "../assets/vieja5.png", "../assets/vieja6.jpg", "../assets/vieja7.jpg", "../assets/vieja8.png"];
+  turno : boolean = true ;
   clicados : number[] = [];
+  opcion : number = 0;
+  finDelJuego : boolean = false;
+  imgGanador : string = "";
+  mensajeGanador : string = "";
 
 
   ponerMarca(num){
     let result = this.clicados.some(pieza => pieza === num);
 
-    console.log(result);
-
-    if (!result) {
+    if (!result && !this.finDelJuego) {
       this.clicados.push(num);
 
       if (this.turno === false) {
-        this.casillas[num] = 2;
+        this.urls[num] = "../assets/FotoCirculo.jpg";
         this.turno = true;
       } else {
-        this.casillas[num] = 1;
+        this.urls[num] = "../assets/FotoX.jpg";
         this.turno = false;
       }
+
+      this.ganador();
     }
+
+    
+  }
+
+  ganador(){
+    let result = this.urls.some(pieza => pieza === "");
+
+    if (this.urls[0] === this.urls[1] && this.urls[0] === this.urls[2] && this.urls[0] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[0])
+    }else if (this.urls[0] === this.urls[3] && this.urls[0] === this.urls[6] && this.urls[0] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[0])
+    }else if (this.urls[0] === this.urls[4] && this.urls[0] === this.urls[8] && this.urls[0] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[0])
+    }else if (this.urls[1] === this.urls[4] && this.urls[1] === this.urls[7] && this.urls[1] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[1])
+    }else if (this.urls[2] === this.urls[5] && this.urls[2] === this.urls[8] && this.urls[2] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[2])
+    }else if (this.urls[2] === this.urls[4] && this.urls[2] === this.urls[6] && this.urls[2] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[2])
+    }else if (this.urls[3] === this.urls[4] && this.urls[3] === this.urls[5] && this.urls[3] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[3])
+    }else if (this.urls[6] === this.urls[7] && this.urls[6] === this.urls[8] && this.urls[6] !== "") {
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(true, this.urls[6])
+    }else if (!result){
+      this.finDelJuego = true;
+      this.mostrarMensajeGanador(false, "");
+    }
+  }
+
+  mostrarMensajeGanador(selector, ganador){
+    if (selector) {
+      this.imgGanador = ganador;
+      this.mensajeGanador = "Felicidades el ganador es ";
+    } else {
+      let num = Math.floor(Math.random() * this.viejas.length);
+      console.log(num);
+      this.imgGanador = this.viejas[num];
+      this.mensajeGanador = "Que mal, la vieja ganó... ";
+    }
+    
+  }
+
+  reiniciar(){
+    this.urls = ["", "", "", "", "", "", "", "", ""];
+    this.finDelJuego = false;
+    this.clicados = [];
+    this.turno = true;
+  }
+
+  seRindio(){
 
   }
 }
